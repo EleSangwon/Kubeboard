919875932a2f68d1a595c0fe4f518a66c2945b16
hsh108868 <pusrty@gmail.com>
2021-09-18 16:30:24 +0900

    node-fetch uninstall

--- web/routes/info.js
+++ web/routes/info.js
@@ -2,16 +2,17 @@ exports.nodeInfo = function (req, res) {
     const os = require("os");
     const fs = require('fs');
 
-    const nodeFile = fs.readFileSync('/client-resource/node_resource.json', 'utf-8').replace(/'/g, "");;
+    const nodeFile = fs.readFileSync('routes/node_resource.json', 'utf-8').replace(/'/g, "");
     const node = JSON.parse(nodeFile);
 
-    const podFile = fs.readFileSync('/client-resource/pod_resource.json', 'utf-8').replace(/'/g, "");
+    const podFile = fs.readFileSync('routes/pod_resource.json', 'utf-8').replace(/'/g, "");
     const pod = JSON.parse(podFile).length;
     
-    const serviceFile = fs.readFileSync('/client-resource/service_resource.json', 'utf-8').replace(/'/g, "");;
+    
+    const serviceFile = fs.readFileSync('routes/service_resource.json', 'utf-8').replace(/'/g, "");
     const service = JSON.parse(serviceFile).length;
 
-    const nsFile = fs.readFileSync('/client-resource/ns_resource.json', 'utf-8').replace(/'/g, "");;
+    const nsFile = fs.readFileSync('routes/ns_resource.json', 'utf-8').replace(/'/g, "");
     const ns = JSON.parse(nsFile).length;
 
     res.render('home.ejs', {
@@ -31,8 +32,7 @@ exports.podInfo = function (req, res) {
     const fs = require('fs');
     const sortJsonArray = require('sort-json-array');
 
-    /*const podFile = fs.readFileSync('routes/pod_resource.json', 'utf-8').replace(/'/g, "");*/
-    const podFile = fs.readFileSync('/client-resource/pod_resource.json', 'utf-8').replace(/'/g, "");
+    const podFile = fs.readFileSync('routes/pod_resource.json', 'utf-8').replace(/'/g, "");
     var pod = JSON.parse(podFile);
 
     pod = sortJsonArray(pod, 'NAMESPACE');
@@ -50,8 +50,7 @@ exports.serviceInfo = function (req, res) {
 
     const fs = require('fs');
 
-    /*const serviceFile = fs.readFileSync('routes/service_resource.json', 'utf-8').replace(/'/g, "");*/
-    const serviceFile = fs.readFileSync('/client-resource/service_resource.json', 'utf-8').replace(/'/g, "");
+    const serviceFile = fs.readFileSync('routes/service_resource.json', 'utf-8').replace(/'/g, "");
     const service = JSON.parse(serviceFile);
 
 
@@ -64,8 +63,7 @@ exports.nsInfo = function (req, res) {
 
     const fs = require('fs');
 
-    /*const nsFile = fs.readFileSync('routes/ns_resource.json', 'utf-8').replace(/'/g, "");*/
-   const nsFile = fs.readFileSync('/client-resource/ns_resource.json', 'utf-8').replace(/'/g, "");
+    const nsFile = fs.readFileSync('routes/ns_resource.json', 'utf-8').replace(/'/g, "");
 
     const ns = JSON.parse(nsFile);
 
@@ -78,8 +76,8 @@ exports.nsInfo = function (req, res) {
 exports.ndInfo = function (req, res) {
     const fs = require('fs');
 
-    /*const nodeFile = fs.readFileSync('routes/node_resource.json', 'utf-8').replace(/'/g, "");*/
-    const nodeFile = fs.readFileSync('/client-resource/node_resource.json', 'utf-8').replace(/'/g, ""); 
+    const nodeFile = fs.readFileSync('routes/node_resource.json', 'utf-8').replace(/'/g, "");
+    /*const nodeFile = fs.readFileSync('/client-resource/node_resource.json', 'utf-8').replace(/'/g, ""); */
     const node = JSON.parse(nodeFile);
 
     res.render('node.ejs', {
@@ -88,42 +86,11 @@ exports.ndInfo = function (req, res) {
 }
 
 exports.errLog = function (req, res) {
-    const fs = require('fs');
     const fetch = require('node-fetch');
-    const Chart = require('chart.js');
 
-    var _html = `<script>ctx = document.getElementById('myChart');
-        var myChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: [...new Set(err.map(it => it.NAMESPACE))],
-            datasets: [{
-                label: '# of Votes',
-                data: [Object.keys(err).length],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }],
-        },
-    }); </script>`
-
+    const response = fetch('https://hanium-test-error-log.s3.ap-northeast-2.amazonaws.com/error.json');
+    const body = response.json();
     res.render('logging.ejs', {
-        errLength: JSON.parse(errFile).length,
-        error: err,
-        html: _html,
+        test : console.log(body),
     })
 }
