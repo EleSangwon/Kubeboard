6b3fe1ae30991f4f2d4e34b2cc6a3467a079ae89
hsh108868 <pusrty@gmail.com>
2021-10-05 22:15:16 +0900

    log-report

--- web/routes/info.js
+++ web/routes/info.js
@@ -7,8 +7,8 @@ exports.nodeInfo = function (req, res) {
 
     const podFile = fs.readFileSync('routes/pod_resource.json', 'utf-8').replace(/'/g, "");
     const pod = JSON.parse(podFile).length;
-    
-    
+
+
     const serviceFile = fs.readFileSync('routes/service_resource.json', 'utf-8').replace(/'/g, "");
     const service = JSON.parse(serviceFile).length;
 
@@ -86,11 +86,70 @@ exports.ndInfo = function (req, res) {
 }
 
 exports.errLog = function (req, res) {
-    const fetch = require('node-fetch');
+    const https = require('https');
+    const ndjson = require('ndjson');
+    const fs = require('fs');
 
-    const response = fetch('https://hanium-test-error-log.s3.ap-northeast-2.amazonaws.com/error.json');
-    const body = response.json();
+    const errorFile = fs.readFileSync('routes/error.json', 'utf-8');
+
+    const error = JSON.parse(errorFile);
+
+    var result = [];
+
+    const url = "https://hanium-test-error-log.s3.ap-northeast-2.amazonaws.com/error.json";
+
+
+    let groupByNamespace = error.reduce((acc, it) =>
+    ({ ...acc, [it.NAMESPACE]: (acc[it.NAMESPACE] || 0) + 1 }),
+    {});
+
+    let groupByName = error.reduce((acc, it) =>
+    ({ ...acc, [it.NAME]: (acc[it.NAME] || 0) + 1 }),
+    {});
+
+    let groupByKind = error.reduce((acc, it) =>
+    ({ ...acc, [it.KIND]: (acc[it.KIND] || 0) + 1 }),
+    {});
+
+    let groupByTime = error.reduce((acc, it) =>
+    ({ ...acc, [it.TIME.slice(0, 10)]: (acc[it.TIME.slice(0, 10)] || 0) + 1 }),
+    {});
+
+    let groupByHost = error.reduce((acc, it) =>
+    ({ ...acc, [it.HOST]: (acc[it.HOST] || 0) + 1 }),
+    {});
+
+
+    /* https.get(url, (stream) => {
+        stream.pipe(ndjson.parse()).on("data", function (data) {
+            result.push(data);
+        }).on("end", () => {
+            let groupByNamespace = result.reduce((acc, it) =>
+                ({ ...acc, [it.NAMESPACE]: (acc[it.NAMESPACE] || 0) + 1 }),
+                {});
+            let groupByName = result.reduce((acc, it) =>
+                ({ ...acc, [it.NAME]: (acc[it.NAME] || 0) + 1 }),
+                {});
+
+            let groupByKind = result.reduce((acc, it) =>
+                ({ ...acc, [it.KIND]: (acc[it.KIND] || 0) + 1 }),
+                {});
+            res.render('logging.ejs', {
+                data: result,
+                namespace: groupByNamespace,
+                name: groupByName,
+                kind: groupByKind,
+            })
+        });
+    }); */
+
     res.render('logging.ejs', {
-        test : console.log(body),
+        data: error,
+        namespace: groupByNamespace,
+        name: groupByName,
+        kind: groupByKind,
+        time: groupByTime,
+        host: groupByHost,
     })
+
 }
