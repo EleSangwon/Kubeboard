a0838802f0d79e4c03e6c30367b42288033da95a
hsh108868 <pusrty@gmail.com>
2021-10-14 20:40:17 +0900

    txt log

--- web-log-error/routes/info.js
+++ web-log-error/routes/info.js
@@ -1,33 +1,27 @@
 exports.errLog = function (req, res) {
   const https = require("https");
-  const ndjson = require("ndjson");
   const fs = require("fs");
+  const ndjsonParser = require("ndjson-parse");
 
-  const errorFile = fs.readFileSync("routes/error.json", "utf-8");
+  const article = fs.readFileSync("routes/error.txt").toString();
+  const parsedArticle = ndjsonParser(article);
 
-  const error = JSON.parse(errorFile);
-
-  var result = [];
-
-  const url =
-    "https://hanium-test-error-log.s3.ap-northeast-2.amazonaws.com/error.json";
-
-  let groupByNamespace = error.reduce(
+  let groupByNamespace = parsedArticle.reduce(
     (acc, it) => ({ ...acc, [it.NAMESPACE]: (acc[it.NAMESPACE] || 0) + 1 }),
     {}
   );
 
-  let groupByName = error.reduce(
+  let groupByName = parsedArticle.reduce(
     (acc, it) => ({ ...acc, [it.NAME]: (acc[it.NAME] || 0) + 1 }),
     {}
   );
 
-  let groupByKind = error.reduce(
+  let groupByKind = parsedArticle.reduce(
     (acc, it) => ({ ...acc, [it.KIND]: (acc[it.KIND] || 0) + 1 }),
     {}
   );
 
-  let groupByTime = error.reduce(
+  let groupByTime = parsedArticle.reduce(
     (acc, it) => ({
       ...acc,
       [it.TIME.slice(0, 10)]: (acc[it.TIME.slice(0, 10)] || 0) + 1,
@@ -35,36 +29,13 @@ exports.errLog = function (req, res) {
     {}
   );
 
-  let groupByHost = error.reduce(
+  let groupByHost = parsedArticle.reduce(
     (acc, it) => ({ ...acc, [it.HOST]: (acc[it.HOST] || 0) + 1 }),
     {}
   );
 
-  /* https.get(url, (stream) => {
-          stream.pipe(ndjson.parse()).on("data", function (data) {
-              result.push(data);
-          }).on("end", () => {
-              let groupByNamespace = result.reduce((acc, it) =>
-                  ({ ...acc, [it.NAMESPACE]: (acc[it.NAMESPACE] || 0) + 1 }),
-                  {});
-              let groupByName = result.reduce((acc, it) =>
-                  ({ ...acc, [it.NAME]: (acc[it.NAME] || 0) + 1 }),
-                  {});
-  
-              let groupByKind = result.reduce((acc, it) =>
-                  ({ ...acc, [it.KIND]: (acc[it.KIND] || 0) + 1 }),
-                  {});
-              res.render('logging.ejs', {
-                  data: result,
-                  namespace: groupByNamespace,
-                  name: groupByName,
-                  kind: groupByKind,
-              })
-          });
-      }); */
-
   res.render("logging.ejs", {
-    data: error,
+    data: parsedArticle,
     namespace: groupByNamespace,
     name: groupByName,
     kind: groupByKind,
