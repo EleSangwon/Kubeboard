f8da9b600a9bacaaf0786100071812b3334a67a9
leesangwon <lee2155507@gmail.com>
2021-10-16 12:31:02 +0900

    Modify web-log-image dir

--- web-log-image/routes/info.js
+++ web-log-image/routes/info.js
@@ -1,14 +1,14 @@
 exports.imgLog = function (req, res) {
   const fs = require("fs");
+  const ndjsonParser = require("ndjson-parse");
+  const article = fs.readFileSync("routes/imagescan.txt").toString();
+  const parsedArticle = ndjsonParser(article);
 
-  /*const article = fs.readFileSync("routes/image-log.txt").toString();
-  const parsedArticle = ndjsonParser(article);*/
-
-  const imgFile = fs.readFileSync("routes/image_log.json", "utf-8");
-  const img = JSON.parse(imgFile);
+//  const imgFile = fs.readFileSync("routes/imagescan.txt", "utf-8");
+//  const img = JSON.parse(imgFile);
 
   res.render("image.ejs", {
-    img: img,
-    /* img : parsedArticle */
+    //img: img,
+    img : parsedArticle
   });
 };
